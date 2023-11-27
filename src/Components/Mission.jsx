ce8ec97575a4826d138a65b45723986119840981
Trevor King <wildbeast447@gmail.com>
2023-11-27 17:40:41 -0500

    Fixed mission card text to be optimized for mobile

--- src/Components/Mission.jsx
+++ src/Components/Mission.jsx
@@ -17,7 +17,7 @@ function Mission() {
             <img className="mission-card-img" src={mission} alt="Peacemakers logo"></img>
             <h1 className="mission-title">Our Mission</h1>
             <p className="mission-para">The Lynchburg Peacemakers mission is to prevent conflicts from escalating into violent acts by providing people with the knowledge and skills needed to resolve disputes peacefully.
-                The Peacemakers conflict resolution program involves increasing problem solving and conflict mediation skills as well as providing community patrols.</p>
+            <br />The Peacemakers conflict resolution program involves increasing problem solving and conflict mediation skills as well as providing community patrols.</p>
         </div>
     );
 }
