20293437d2472e9607bb582f243e1c198bf33e8d
Trevor King <wildbeast447@gmail.com>
2023-11-27 13:17:26 -0500

    fixed slider

--- src/Components/Mission.jsx
+++ src/Components/Mission.jsx
@@ -5,11 +5,7 @@ import mission from '../assets/logo.jpg';
 
 function Mission() {
     return(
-<<<<<<< HEAD
         <div id="Mission" className="mission-card">
-=======
-        <div className="mission-card">
->>>>>>> dfe3d6552db3f3cba18efd422ac6b6f3300db809
             <img className="mission-card-img" src={mission} alt="Peacemakers logo"></img>
             <h1 className="mission-title">Our Mission</h1>
             <p className="mission-para">The Lynchburg Peacemakers mission is to prevent conflicts from escalating into violent acts by providing people with the knowledge and skills needed to resolve disputes peacefully.
