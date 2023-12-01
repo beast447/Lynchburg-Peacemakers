b06d0805524dcd828dc143d9b477549fab801c57
Trevor King <wildbeast447@gmail.com>
2023-12-01 13:00:16 -0500

    added more footer information

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,6 +3,7 @@ function Footer()  {
     return (
         <footer>
             <p>© 2023 Lynchburg Peacemakers</p>
+            <p>A 501(c)3 nonprofit organization in Lynchburg, VA</p>
         </footer>
     )
 }
