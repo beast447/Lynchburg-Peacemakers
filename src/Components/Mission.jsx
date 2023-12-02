2462f804b8934354c6373d7ccece3186f75a255d
Trevor King <wildbeast447@gmail.com>
2023-12-02 11:28:15 -0500

    Added the ability for the mission card to flip and display services

--- src/Components/Mission.jsx
+++ src/Components/Mission.jsx
@@ -11,15 +11,34 @@ function Mission() {
     const handleCardClick = () => {
         setIsFlipped(!isFlipped);
     }
-
-    return(
-        <div id="Mission" className="mission-card-front">
-            <img className="mission-card-img" src={mission} alt="Peacemakers logo"></img>
-            <h1 className="mission-title">Our Mission</h1>
-            <p className="mission-para">The Lynchburg Peacemakers mission is to prevent conflicts from escalating into violent acts by providing people with the knowledge and skills needed to resolve disputes peacefully.
-            <br />The Peacemakers conflict resolution program involves increasing problem solving and conflict mediation skills as well as providing community patrols.</p>
-        </div>
+    if (isFlipped === true) {
+        return(
+            <div id="Mission" className="mission-card-front">
+                <h3 className="click-logo">Click the logo to learn more!</h3>
+                <img className="mission-card-img" src={mission} onClick={handleCardClick} alt="Peacemakers logo"></img>
+                <h1 className="mission-title">Our Mission</h1>
+                <p className="mission-para">The Lynchburg Peacemakers mission is to prevent conflicts from escalating into violent acts by providing people with the knowledge and skills needed to resolve disputes peacefully.
+                <br />The Peacemakers conflict resolution program involves increasing problem solving and conflict mediation skills as well as providing community patrols.</p>
+            </div>
     );
+    } else { 
+        return(
+            <div id="Mission" className="mission-card-back">
+                <img className="mission-card-img" src={mission} onClick={handleCardClick} alt="Peacemakers logo"></img>
+                <h1 className="services-title">Our Services</h1>
+                <div className="services-list">
+                    <p>Assist in entering a trade school</p>
+                    <p>Assist in entering a higher learning institution</p>
+                    <p>Assist in finding employment</p>
+                    <p>Assist in finding housing</p>
+                    <p>Parental Counseling</p>
+                    <p>Conflict Resolution Training</p>
+                    <p>CPR and First Aid classes</p>
+                </div>
+            </div>
+        );
+
+    }
 }
 
 export default Mission;
