81d542e5b00be5f5c3f89ebf6b81ea1a3dd2cf68
Trevor King <wildbeast447@gmail.com>
2024-02-13 12:47:45 -0500

    added an address on donation tile

--- src/Components/Donate.jsx
+++ src/Components/Donate.jsx
@@ -9,6 +9,8 @@ function Donate(){
         <div id="Donate" className="donate">
             <h1>Donate Today</h1>
             <img className="donate-img" src={DonateImage} alt="Donate"/>
+            <h2>We are now also accepting mailed checks or money orders at:</h2>
+            <h3>Lynchburg Peacemakers Inc. 1601 12th Street, Lynchburg, VA, 24501</h3>
         </div>
     );
 }
